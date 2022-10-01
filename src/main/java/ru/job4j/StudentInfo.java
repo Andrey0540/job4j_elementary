e2cd0cd10dc6bd451c2baa5dc868390bd8d2d171
Andrey0540 <freebiker@mail.ru>
2022-10-01 11:48:36 +0700

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Андрей");
+        System.out.println("11.08.1984");
+
+    }
+}
